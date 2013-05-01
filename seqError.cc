570da52a10380695eaa506efb413bd6d173be6b0
Avinash <trackavinash@gmail.com>
2013-05-01 14:07:37 -0500

    fairly stable, reverted to decimal space, fixed bug in mismatch counts

--- seqError.cc
+++ seqError.cc
@@ -22,10 +22,14 @@
 #include<sstream>
 #include<map>
 #include<cstdlib>
+#include<cmath>
+
+#define RD_CUTOFF 10 //process pileup of lines with RD > RD_CUTOFF
+#define MIN_TRIPLET_COUNT 10 // minimum triplet count for a triplet to be output.
 using namespace std;
 
-//std::map<std::string, float> triplet_cumul_match_fraction;
-std::map<std::string, float> triplet_cumul_mismatch_fraction;
+std::map<std::string, float> triplet_cumul_mismatch_counts;
+std::map<std::string, double> triplet_cumul_mismatch_fraction;
 std::map<std::string, long> triplet_count;
 int g_indel_count = 0;
 
@@ -38,14 +42,20 @@ struct line_object{
   string read_bases;
 };
 
-void print_map_cumul_normalized_mismatches()
+void print_op()
 {
-  cout<<"In print map kmer cumul mismatch."<<endl;
-  std::map<std::string, float>::iterator it;
+  cout<<"Triplet\tTriplet_cumul_mismatch_count\tTriplet_cumul_mismatch_fraction\tTriplet_count\tNormalized_mismatch_fraction\n";
+  std::map<std::string, double>::iterator it;
   for(it = triplet_cumul_mismatch_fraction.begin();
       it != triplet_cumul_mismatch_fraction.end();
       it++) {
-    cout<<it->first<<"\t"<<it->second<<"\t"<<it->second/triplet_count[it->first]<<endl;
+    if(triplet_count[it->first] > MIN_TRIPLET_COUNT) {
+      cout<<it->first;
+      cout<<"\t"<<triplet_cumul_mismatch_counts[it->first];
+      cout<<"\t"<<it->second<<"\t"<<triplet_count[it->first];
+      cout<<"\t"<<it->second/triplet_count[it->first]<<endl;
+      //    cout<<"\t"<<exp(it->second/triplet_count[it->first]); //antilog
+    }
   }
 }
 
@@ -114,8 +124,11 @@ void parse_line_object(string line, line_object& lo1)
   iss >> lo1.pos;
 
   iss >> lo1.fwd_ref_base;
-  if(lo1.fwd_ref_base > 97)
+  if(lo1.fwd_ref_base >= 97)
     lo1.fwd_ref_base -= 32;
+#ifdef DEBUG
+  cout<<"ref"<<lo1.fwd_ref_base<<endl;
+#endif
   iss >> lo1.read_depth;
   iss >> lo1.read_bases;
   lo1.rev_ref_base = complement_DNA(lo1.fwd_ref_base);
@@ -134,9 +147,10 @@ void process_read_bases(string read_bases, const char* fwd_triplet, char* rev_tr
   
   if(read_bases.find('+') != std::string::npos || read_bases.find('-') != std::string::npos) {
 #ifdef DEBUG
-    cout<<endl<<"INDEL "<<read_bases<<" indel_count "<<++g_indel_count;
+    cout<<endl<<"INDEL "<<read_bases<<" indel_count "<<g_indel_count+1;
 #endif
-      return;
+    ++g_indel_count;
+    return;
   }
 
   for(int pos = 0; pos<read_bases.length(); pos++) {
@@ -160,18 +174,28 @@ void process_read_bases(string read_bases, const char* fwd_triplet, char* rev_tr
   cout<<endl<<"rev match "<<rev_match<<"\t"<<" rev_mismatch "<<rev_mismatch<<endl;
 #endif
 
-  if(fwd_match + fwd_mismatch != 0)
-    triplet_cumul_mismatch_fraction[fwd_triplet] += float(fwd_mismatch)/float(fwd_match + fwd_mismatch);
+  if(fwd_match + fwd_mismatch != 0 && fwd_mismatch != 0)
+     triplet_cumul_mismatch_fraction[fwd_triplet] += float(fwd_mismatch)/float(fwd_match + fwd_mismatch);
+    // triplet_cumul_mismatch_fraction[fwd_triplet] += log(float(fwd_mismatch)) - log(float(fwd_match + fwd_mismatch)); /*log space*/
   else
-    triplet_cumul_mismatch_fraction[fwd_triplet] = 0;
+    triplet_cumul_mismatch_fraction[fwd_triplet] += 0;
+    // triplet_cumul_mismatch_fraction[fwd_triplet] += -255; /*log space*/
+  
 
-  if(rev_match + rev_mismatch != 0)
+  if(rev_match + rev_mismatch != 0 && rev_mismatch !=0)
     triplet_cumul_mismatch_fraction[rev_triplet] += float(rev_mismatch)/float(rev_match + rev_mismatch);
+    // triplet_cumul_mismatch_fraction[rev_triplet] += log(float(rev_mismatch)) - log(float(rev_match + rev_mismatch)); /*log space*/
   else 
-    triplet_cumul_mismatch_fraction[rev_triplet] = 0;
+    triplet_cumul_mismatch_fraction[rev_triplet] += 0;
+    // triplet_cumul_mismatch_fraction[rev_triplet] += -255; /*log space*/
 
   triplet_count[fwd_triplet] += 1;
   triplet_count[rev_triplet] += 1;
+
+  triplet_cumul_mismatch_counts[fwd_triplet] += fwd_mismatch;
+  triplet_cumul_mismatch_counts[rev_triplet] += rev_mismatch;
+  
+
 }
 
 
@@ -182,25 +206,32 @@ void process_mpileup()
   line_object current_line_object, next1_line_object, next2_line_object;
   char  fwd_triplet[3], rev_triplet[3];
   string old_chr = "NA";
-
+  long first_pos = 0, last_pos = 0;
   int line_number = 0;
+  bool first_line = true;
+
   if(!getline(cin, current_line))
     exit(EXIT_FAILURE);
   if(!getline(cin, next1_line)) 
     exit(EXIT_FAILURE);
+
+
   while(getline(cin, next2_line)) {
+
     line_number++;
     
     parse_line_object(current_line, current_line_object);
+    if(first_line) {
+      first_pos = current_line_object.pos;
+      first_line = false;
+    }
     parse_line_object(next1_line, next1_line_object);
     parse_line_object(next2_line, next2_line_object);
 #ifdef DEBUG    
     cout<<current_line_object.fwd_ref_base<<endl;
 #endif
-
     if(current_line_object.chr != old_chr)
       line_number = 1;
-    
     if(line_number > 2) {
       fwd_triplet[2]= current_line_object.fwd_ref_base;
       rev_triplet[2]= current_line_object.rev_ref_base;
@@ -208,7 +239,8 @@ void process_mpileup()
       rev_triplet[0]= next2_line_object.rev_ref_base;
       fwd_triplet[3] = '\0';
       rev_triplet[3] = '\0';
-      process_read_bases(current_line_object.read_bases, fwd_triplet, rev_triplet);
+      if(current_line_object.read_depth > RD_CUTOFF)
+	process_read_bases(current_line_object.read_bases, fwd_triplet, rev_triplet);
       fwd_triplet[0] = fwd_triplet[1];
       fwd_triplet[1] = fwd_triplet[2];
     }
@@ -219,6 +251,8 @@ void process_mpileup()
     current_line = next1_line;
     next1_line = next2_line;
   };
+  last_pos = current_line_object.pos;
+  cerr<<"first_pos: "<<first_pos<<"\tlast_pos: "<<last_pos<<endl;
 }
 
 
@@ -227,9 +261,9 @@ void process_mpileup()
 int main(int argc, char* argv[])
 {
   process_mpileup();
-  print_map_cumul_normalized_mismatches();
-  print_map_kmer_counts();
-  cout<<endl<<"The total number of indels is "<<g_indel_count;
-  cout<<endl<<"Done!";
+  print_op();
+  //  print_map_kmer_counts();
+  cerr<<endl<<"The total number of indels is "<<g_indel_count;
+  cerr<<endl<<"Done!";
   exit(EXIT_SUCCESS);
 }
